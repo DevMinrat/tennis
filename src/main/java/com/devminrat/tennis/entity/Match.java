041050af6fae196036e8f097e498040aea94fa5f
DevMinrat <devminrat@gmail.com>
2024-09-17 22:51:54 +0300

    feat: add h2 dependency, add hibernate.cfg.xml, create Player and Match entities

---
+++ src/main/java/com/devminrat/tennis/entity/Match.java
@@ -0,0 +1,93 @@
+package com.devminrat.tennis.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Match {
+    @Id
+    private int id;
+    private int player1;
+    private int player2;
+    private int winner;
+
+    public Match() {
+    }
+
+    public Match(int id, int player1, int player2, int winner) {
+        this.id = id;
+        this.player1 = player1;
+        this.player2 = player2;
+        this.winner = winner;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPlayer1() {
+        return player1;
+    }
+
+    public void setPlayer1(int player1) {
+        this.player1 = player1;
+    }
+
+    public int getPlayer2() {
+        return player2;
+    }
+
+    public void setPlayer2(int player2) {
+        this.player2 = player2;
+    }
+
+    public int getWinner() {
+        return winner;
+    }
+
+    public void setWinner(int winner) {
+        this.winner = winner;
+    }
+
+    public static class MatchBuilder {
+        private int id;
+        private int player1;
+        private int player2;
+        private int winner;
+
+        public MatchBuilder() {
+        }
+
+        public MatchBuilder id(int id) {
+            this.id = id;
+            return this;
+        }
+
+        public MatchBuilder player1(int player1) {
+            this.player1 = player1;
+            return this;
+        }
+
+        public MatchBuilder player2(int player2) {
+            this.player2 = player2;
+            return this;
+        }
+
+        public MatchBuilder winner(int winner) {
+            this.winner = winner;
+            return this;
+        }
+
+        public Match build() {
+            return new Match(id, player1, player2, winner);
+        }
+    }
+
+    public static MatchBuilder builder() {
+        return new MatchBuilder();
+    }
+}
---
+++ src/main/java/com/devminrat/tennis/entity/Player.java
@@ -0,0 +1,38 @@
+package com.devminrat.tennis.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Player {
+    @Id
+    private Integer id;
+    private String name;
+
+    public Player() {
+    }
+
+    public Player(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
